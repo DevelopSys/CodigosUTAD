87d29959cf3459e7fb446f9cae309dc33848bf29
Develop and System <developandsystem@gmail.com>
2024-03-18 10:47:54 +0100

    update

--- 2324/MAIS3/T3/Comunicacion/app/src/main/java/com/example/comunicacion/data/DataSet.kt
+++ 2324/MAIS3/T3/Comunicacion/app/src/main/java/com/example/comunicacion/data/DataSet.kt
@@ -1,7 +1,9 @@
 package com.example.comunicacion.data
 
+import android.view.Display.Mode
 import com.example.comunicacion.R
 import com.example.comunicacion.model.Marca
+import com.example.comunicacion.model.Modelo
 
 class DataSet {
 
@@ -22,6 +24,22 @@ class DataSet {
 
             return listaMarcar
 
+        }
+
+        fun getAllModelos(marcaSeleccionada: String) : ArrayList<Modelo>{
+            val listaModelos = ArrayList<Modelo>();
+            listaModelos.add(Modelo("Mercedes","C220",200,50000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("Mercedes","GLC",400,80000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("Audi","A8",500,90000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("Audi","E-Tron",300,70000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("Ford","Mustang",450,60000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("Ford","Focus",200,30000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("VW","Arteon",200,50000.0,R.drawable.mercedes))
+            listaModelos.add(Modelo("VW","Taigo",200,60000.0,R.drawable.mercedes))
+
+            return listaModelos.filter {
+                return@filter it.marca.equals(marcaSeleccionada)
+            } as ArrayList<Modelo>;
         }
     }
 
--- 2324/MAIS3/T3/Comunicacion/app/src/main/java/com/example/comunicacion/ui/activity/MainActivity.kt
+++ 2324/MAIS3/T3/Comunicacion/app/src/main/java/com/example/comunicacion/ui/activity/MainActivity.kt
@@ -2,45 +2,41 @@ package com.example.comunicacion.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.comunicacion.R
+import com.example.comunicacion.adapters.ModeloAdapter
 import com.example.comunicacion.data.DataSet
 import com.example.comunicacion.databinding.ActivityMainBinding
 import com.example.comunicacion.model.Marca
+import com.example.comunicacion.model.Modelo
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adaptadorMarcas: ArrayAdapter<Marca>
     private lateinit var correo: String
+    private lateinit var adaptadorModelos: ModeloAdapter
     private var perfil: Char? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        adaptadorMarcas = ArrayAdapter(
-            applicationContext,
-            android.R.layout.simple_spinner_item,
-            DataSet.getAllMarcas()
-        )
-
-        adaptadorMarcas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerMarca.adapter = adaptadorMarcas
+        instancias()
+        personalizarSpinner()
+        personalizarRecycler()
 
         correo = intent.getStringExtra("correo")!!
         perfil = intent.getStringExtra("perfil")!!.get(0)
-
         binding.textoSaludo.setText("Bienvenido $correo")
         binding.textoPerfil.setText("$perfil")
-
-
 
 
         binding.spinnerMarca.onItemSelectedListener = object : OnItemSelectedListener {
@@ -50,10 +46,12 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                // Log.v("datos", parent!!.adapter.getItem(position).toString())
-                val marca: Marca = parent!!.adapter.getItem(position) as Marca
-                Snackbar.make(binding.root, "${marca.nombre}, calidad: ${marca.calidad}", Snackbar.LENGTH_SHORT).show()
-                binding.imagenMarca.setImageResource(marca.imagen)
+
+                val seleccion = parent!!.adapter.getItem(position) as Marca
+                // al adaptador le tendré que dar la lista nueva
+                adaptadorModelos.getListaModelos(DataSet.getAllModelos(seleccion.nombre))
+
+
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -62,7 +60,26 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
 
+    fun instancias(){
+        adaptadorModelos = ModeloAdapter(DataSet.getAllModelos("Mercedes"), this)
+        adaptadorMarcas = ArrayAdapter(
+            applicationContext,
+            android.R.layout.simple_spinner_item,
+            DataSet.getAllMarcas()
+        )
+
+    }
+    fun personalizarRecycler(){
+        binding.recyclerModelos.adapter = adaptadorModelos
+        binding.recyclerModelos.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+    }
+    fun personalizarSpinner(){
+        adaptadorMarcas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerMarca.adapter = adaptadorMarcas
     }
 
 
