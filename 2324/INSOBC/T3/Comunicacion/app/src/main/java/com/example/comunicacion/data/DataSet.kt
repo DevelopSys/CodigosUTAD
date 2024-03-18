a1c90ede705ebfc9b54868079793c5f3f31b2c68
Develop and System <developandsystem@gmail.com>
2024-03-18 12:45:36 +0100

    update

--- 2324/INSOBC/T3/Comunicacion/app/src/main/java/com/example/comunicacion/data/DataSet.kt
+++ 2324/INSOBC/T3/Comunicacion/app/src/main/java/com/example/comunicacion/data/DataSet.kt
@@ -1,22 +1,40 @@
 package com.example.comunicacion.data
 
+import android.view.Display.Mode
 import com.example.comunicacion.R
 import com.example.comunicacion.model.Marca
+import com.example.comunicacion.model.Modelo
 
 class DataSet {
 
     // el conjunto de metodos que PERTENECEN a la clase (no al objeto)
     // y al llamarlos me dan el conjundo de datos
     // DataSet.metodo()
-    companion object{
+    companion object {
         fun getAllMarcas(): ArrayList<Marca> {
-
             val lista: ArrayList<Marca> = ArrayList()
-            lista.add(Marca("Mercedes","Premium", R.drawable.mercedes))
-            lista.add(Marca("Audi","Premium", R.drawable.audi))
-            lista.add(Marca("VW","Alta", R.drawable.vw))
-            lista.add(Marca("Ford","Normal", R.drawable.ford))
+            lista.add(Marca("Mercedes", "Premium", R.drawable.mercedes))
+            lista.add(Marca("Audi", "Premium", R.drawable.audi))
+            lista.add(Marca("VW", "Alta", R.drawable.vw))
+            lista.add(Marca("Ford", "Normal", R.drawable.ford))
             return lista
+        }
+
+        fun getAllModelos(marca: String): List<Modelo> {
+
+            val lista = ArrayList<Modelo>()
+            lista.add(Modelo("Mercedes", "C220", 60000.0, 300, R.drawable.vw))
+            lista.add(Modelo("Mercedes", "GLC", 80000.0, 450, R.drawable.vw))
+            lista.add(Modelo("Audi", "A8", 100000.0, 500, R.drawable.vw))
+            lista.add(Modelo("Audi", "E-Tron", 120000.0, 400, R.drawable.vw))
+            lista.add(Modelo("Ford", "Mustang", 60000.0, 300, R.drawable.vw))
+            lista.add(Modelo("Ford", "Focus", 30000.0, 200, R.drawable.vw))
+            lista.add(Modelo("VW", "Arteon", 60000.0, 300, R.drawable.vw))
+            lista.add(Modelo("VW", "TROC", 50000.0, 200, R.drawable.vw))
+
+            return lista.filter {
+                return@filter it.marca.equals(marca)
+            }
         }
     }
 
--- 2324/INSOBC/T3/Comunicacion/app/src/main/java/com/example/comunicacion/ui/activity/MainActivity.kt
+++ 2324/INSOBC/T3/Comunicacion/app/src/main/java/com/example/comunicacion/ui/activity/MainActivity.kt
@@ -1,35 +1,37 @@
 package com.example.comunicacion.ui.activity
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.comunicacion.R
+import com.example.comunicacion.adapters.AdaptadorModelo
 import com.example.comunicacion.data.DataSet
 import com.example.comunicacion.databinding.ActivityMainBinding
 import com.example.comunicacion.model.Marca
+import com.example.comunicacion.model.Modelo
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adaptadorMarcas: ArrayAdapter<Marca>
+    private lateinit var adaptadorModelo: AdaptadorModelo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        adaptadorMarcas = ArrayAdapter(
-            applicationContext,
-            android.R.layout.simple_spinner_item,
-            DataSet.getAllMarcas()
-        )
-
-        adaptadorMarcas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerMarca.adapter = adaptadorMarcas
+        instancias()
+        persoSpinner()
+        persoRecycler()
 
         val correo = intent.getStringExtra("correo")!!
         val perfil = intent.getStringExtra("perfil")!!
@@ -37,23 +39,31 @@ class MainActivity : AppCompatActivity() {
         binding.textoSaludo.setText("Bienvenido $correo")
         binding.textoPefil.setText("$perfil")
 
+        acciones()
+
+    }
+
+    fun acciones() {
         binding.agregarMarca.setOnClickListener {
-            adaptadorMarcas.add(Marca("Nueva","Desconocido",R.drawable.user))
-            adaptadorMarcas.notifyDataSetChanged()
+            // adaptadorMarcas.add(Marca("Nueva", "Desconocido", R.drawable.user))
+            // adaptadorMarcas.notifyDataSetChanged()
+            adaptadorModelo.addModelo(Modelo("Mercedes", "GLE", 70000.0, 300,
+                R.drawable.mercedes))
+
         }
-
-
         binding.spinnerMarca.onItemSelectedListener = object : OnItemSelectedListener {
             override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
             ) {
                 val seleccion = parent!!.adapter.getItem(position) as Marca
-                binding.imagenMarca.setImageResource(seleccion.imagen)
-                //binding.imagenMarca.setImageDrawable(resources.getDrawable(R.drawable.mercedes,null))
-                Snackbar.make(
+                //seleccion.nombre
+
+                // filtrar los datos
+                // ir al dataset y filtrar por nombre
+                // modificar la lista -> desde el adaptador
+                adaptadorModelo.cambiarLista(DataSet.getAllModelos(seleccion.nombre))
+
+                /*Snackbar.make(
                     binding.root,
                     "${resources.getString(R.string.nombre)}: ${seleccion.nombre}, ${
                         resources.getString(
@@ -61,7 +71,7 @@ class MainActivity : AppCompatActivity() {
                         )
                     }: ${seleccion.calidad}",
                     Snackbar.LENGTH_SHORT
-                ).show()
+                ).show()*/
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -70,6 +80,33 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    fun persoSpinner() {
+        adaptadorMarcas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerMarca.adapter = adaptadorMarcas
+    }
+
+    fun persoRecycler() {
+        binding.recyclerModelos.adapter = adaptadorModelo
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            binding.recyclerModelos.layoutManager = GridLayoutManager(this, 2)
+        } else {
+            binding.recyclerModelos.layoutManager =
+                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        }
+
+        /*LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL, false)*/
+    }
+
+    fun instancias() {
+
+        adaptadorModelo = AdaptadorModelo(DataSet.getAllModelos("Mercedes"), this)
+        adaptadorMarcas = ArrayAdapter(
+            applicationContext, android.R.layout.simple_spinner_item, DataSet.getAllMarcas()
+        )
     }
 
 
