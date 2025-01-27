2f7c681f5f31a73e7df1792e0540fbd2911cfffd
Develop and System <developandsystem@gmail.com>
2025-01-27 10:49:23 +0100

    update

---
+++ 2425/MAISFII/Inicio/src/Entrada.kt
@@ -0,0 +1,41 @@
+// variable
+// metodo
+// clase
+// public static void main(String[] args)
+
+// fun nombreMetodo(param:Tipo, param:Tipo){
+// return 7
+// }
+fun main() {
+    /*
+    // no mutable val nombre: Tipo = valor
+    val DNI: String = "1234A"
+    // mutables var edad: Tipo = valor
+    var nombre: String = "Borja"
+    var apellido: String? = "Martin"
+    lateinit var asignatura: String
+    var edad: Int = 40
+    var experiencia: Boolean = true
+    println("Mi nombre es $nombre, mi edad es $edad")
+    println("Mi apellido es ${apellido}")
+    asignatura = "Desarrollo movil"
+    println("La asignatura que imparto es ${asignatura}")*/
+    saludar(veces = 1)
+
+}
+
+fun recorrerElementos(){
+    // Array -> estático
+    // ArrayList -> dinámico
+    val elementosArray: Array<String> = arrayOf("Palabra1", "Palabra2")
+    // val elementos: ArrayList<String> = arrayListOf("asdasdasd")
+
+
+}
+
+fun saludar(nombre: String?=null, veces: Int){
+
+    for (i in 0..veces-1){
+        println("Hola ${nombre?:"sin nombre"} este es tu primer programa Kotlin")
+    }
+}
---
+++ 2425/MAISFII/Inicio/src/Usuario.kt
@@ -0,0 +1,10 @@
+class Usuario() {
+    // variables
+    // constructores
+        // primarios -> base
+        // secundarios -> son aquellos que aportan datos adicionales, basados en los primarios
+        constructor(nombre: String): this(){
+
+        }
+    // metodos
+}
