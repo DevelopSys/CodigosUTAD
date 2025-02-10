927b9a0bc6bed76d79840892d298717d4cac79f5
Develop and System <developandsystem@gmail.com>
2025-02-10 10:50:17 +0100

    update

--- 2425/MAISFII/T2/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ 2425/MAISFII/T2/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.inicio
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +13,11 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        acciones()
         /*binding.botonSaludar.setOnClickListener {
 
             if (binding.editNombre.text.isEmpty()) {
@@ -36,4 +37,30 @@ class MainActivity : AppCompatActivity() {
         }*/
 
     }
+
+    private fun acciones() {
+        binding.checkInicio.setOnCheckedChangeListener { _, b ->
+            binding.btnLogin.isEnabled = b
+        }
+
+        binding.btnLogin.setOnClickListener {
+            if (!binding.editCorreo.text.isEmpty()
+                && !binding.editPass.text.isEmpty()){
+
+                Log.v("datos",binding.editCorreo.text.toString())
+                Log.v("datos",binding.editPass.text.toString())
+                Log.v("datos",binding.checkInicio.isChecked.toString())
+                Log.v("datos",binding.spinnerPerfil.selectedItem.toString())
+
+                // cambio pantalla
+                val intent = Intent(applicationContext, SecondActivity::class.java)
+                startActivity(intent)
+
+            } else {
+
+            }
+        }
+    }
+
+
 }
