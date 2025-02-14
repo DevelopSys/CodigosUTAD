759ceb9f0a2ef828d315fcdc8ebbf96ca8633691
Develop and System <developandsystem@gmail.com>
2025-02-14 14:37:39 +0100

    update

--- 2425/INSOA/T2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ 2425/INSOA/T2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.inicio
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val correo = binding.editCorreo.text.toString()
+            val pass = binding.editPass.text.toString()
+            val perfil = binding.spinnerPerfil.selectedItem.toString()
+
+            if (correo.equals("admin") && pass.equals("admin")){
+                val intent: Intent = Intent(applicationContext,SecondActivity::class.java)
+                startActivity(intent)
+            } else {
+                Snackbar.make(binding.root,
+                    "Fallo en login",Snackbar.LENGTH_SHORT).show()
+            }
+            /*Log.v("datos",correo)
+            Log.v("datos",pass)
+            Log.v("datos",perfil)*/
+
+        }
+        binding.btnRegistro.setOnClickListener {  }
+        binding.checkBox.setOnCheckedChangeListener { _, b ->
+            binding.btnLogin.isEnabled = b
+        }
 
 
     }
