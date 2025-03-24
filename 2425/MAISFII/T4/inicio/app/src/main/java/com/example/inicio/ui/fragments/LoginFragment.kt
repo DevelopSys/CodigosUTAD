82eafc026b0834a906dacc1047183624535d6ee0
Develop and System <developandsystem@gmail.com>
2025-03-24 09:00:26 +0100

    update

--- 2425/MAISFII/T4/inicio/app/src/main/java/com/example/inicio/ui/fragments/LoginFragment.kt
+++ 2425/MAISFII/T4/inicio/app/src/main/java/com/example/inicio/ui/fragments/LoginFragment.kt
@@ -7,25 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.inicio.R
+import com.example.inicio.databinding.FragmentLogBinding
 import com.example.inicio.databinding.FragmentLoginBinding
 
 
 class LoginFragment: Fragment() {
 
-    private lateinit var binding: FragmentLoginBinding
+    private lateinit var binding: FragmentLogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentLogBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-        binding.botonRegistro.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             val bundle = Bundle()
             bundle.putString("dato","dato que voy a pasar entre fragments")
             findNavController().navigate(R.id.action_loginFragment_to_registroFragment,bundle)
