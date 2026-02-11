66c9edfe0ae7d7a48d7964c4db0cca6025559431
Develop and System <developandsystem@gmail.com>
2026-02-11 12:55:15 +0100

    update

---
+++ 2526/INSOD/TEMA2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.inicio
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inicio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(){
+    // primer metodo del ciclo de vida
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.botonSaludar.setOnClickListener {
+
+        }
+
+    }
+}
