c3e5f5a080e62e653da8a06c33fd8e53d0488ab7
Develop and System <developandsystem@gmail.com>
2026-02-16 12:40:46 +0100

    update

--- 2526/INSOA/TEMA2/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ 2526/INSOA/TEMA2/Inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.inicio
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.inicio.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -15,7 +18,86 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.v("ciclo_vida", "ejcutando el onCreate")
+    }
 
+    override fun onResume() {
+        super.onResume()
 
+        /*
+        binding.botonSaludar.setOnClickListener {
+
+            val nombre: String = binding.editNombre.text.toString()
+
+            if (nombre.isEmpty()){
+                Snackbar.make(
+                    binding.root,
+                    R.string.text_empty_name,
+                    Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(
+                    binding.root,
+                    "${resources.getString(R.string.text_congratulation)} " +
+                            "$nombre, has completado el primer proyecto",
+                    Snackbar.LENGTH_LONG).show()
+            }
+
+        }
+        binding.botonVaciar.setOnClickListener {  }
+         */
+        binding.botonSaludar.setOnClickListener(this)
+        binding.botonVaciar.setOnClickListener(this)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("ciclo_vida", "ejcutando el onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v("ciclo_vida", "ejcutando el onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("ciclo_vida", "ejcutando el onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("ciclo_vida", "ejcutando el onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("ciclo_vida", "ejcutando el onDestroy")
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            binding.botonVaciar.id -> {
+                binding.editNombre.text.clear()
+            }
+            binding.botonSaludar.id -> {
+                val nombre: String = binding.editNombre.text.toString()
+
+                if (nombre.isEmpty()) {
+                    Snackbar.make(
+                        binding.root,
+                        R.string.text_empty_name,
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                } else {
+                    Snackbar.make(
+                        binding.root,
+                        "${resources.getString(R.string.text_congratulation)} " +
+                                "$nombre, has completado el primer proyecto",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
     }
 }
--- 2526/INSOBC/TEMA2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ 2526/INSOBC/TEMA2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -1,23 +1,98 @@
 package com.example.inicio
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.inicio.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     // tantas variables coo elementos graficos tenga
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+        actions()
+        Log.v("ciclo_vida", "metodo onCreate ejecutado")
+    }
 
+    private fun actions() {
+        /*
+        binding.botonSaludar.setOnClickListener {
+
+            val nombre = binding.editNombre.text.toString()
+
+            if (nombre.isEmpty()) {
+                Snackbar.make(
+                    binding.root, R.string.text_snack_empty, Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                // quien ha generado el evento -> it
+                Snackbar.make(
+                    binding.root,
+                    "${resources.getString(R.string.text_congrats)} $nombre has completado la primera app",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+
+
+        }
+        binding.botonVaciar.setOnClickListener {
+            binding.editNombre.text.clear()
+        }*/
+
+        binding.botonVaciar.setOnClickListener(this)
+        binding.botonSaludar.setOnClickListener(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v("ciclo_vida", "metodo onStart ejecutado")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v("ciclo_vida", "metodo onResume ejecutado")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v("ciclo_vida", "metodo onPause ejecutado")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v("ciclo_vida", "metodo onStop ejecutado")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v("ciclo_vida", "metodo onDestroy ejecutado")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v("ciclo_vida", "metodo onRestart ejecutado")
+    }
+
+    override fun onClick(v: View?) {
+        // que pasa cuando se pulsa el boton
+            // cual de los dos es el pulsado
+        when(v!!.id){
+           binding.botonSaludar.id->{
+
+           }
+           binding.botonVaciar.id->{
+
+           }
+        }
     }
 }
