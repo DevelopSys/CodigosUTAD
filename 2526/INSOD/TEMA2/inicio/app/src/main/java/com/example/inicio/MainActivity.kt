201753ab06d3e6c5f935aa6346aaad0e2d34f642
Develop and System <developandsystem@gmail.com>
2026-02-18 12:50:50 +0100

    update

--- 2526/INSOD/TEMA2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
+++ 2526/INSOD/TEMA2/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -8,8 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.inicio.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     // primer metodo del ciclo de vida
 
     private lateinit var binding: ActivityMainBinding
@@ -19,9 +20,49 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        actions()
+
+
+    }
+
+    private fun actions() {
+
+        /*
+        binding.botonVaciar.setOnClickListener {
+            binding.editNombre.text.clear()
+        }
         binding.botonSaludar.setOnClickListener {
 
+            if (binding.editNombre.text.isEmpty()) {
+                showSnack(resources.getString(R.string.text_snack_empty))
+            } else {
+                showSnack("Enhorabuena ${binding.editNombre.text.toString()}, reto comletado")
+            }
         }
+        */
 
+        binding.botonSaludar.setOnClickListener(this)
+        binding.botonVaciar.setOnClickListener(this)
+    }
+
+    private fun showSnack(text: String) {
+        val snack = Snackbar.make(
+            binding.root,
+            text,
+            Snackbar.LENGTH_LONG
+        )
+        snack.show()
+    }
+
+    override fun onClick(v: View?) {
+       // funcionamiento comun
+        when(v?.id){
+           binding.botonVaciar.id->{
+
+           }
+           binding.botonSaludar.id->{
+
+           }
+       }
     }
 }
