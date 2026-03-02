bf4afb994bc3da463eb64dd9cbc6c3fc7da7ec61
Develop and System <developandsystem@gmail.com>
2026-03-02 09:41:10 +0100

    update

--- 2526/INSOA/TEMA2/Inicio/app/src/main/java/com/example/inicio/LoginActivity.kt
+++ 2526/INSOA/TEMA2/Inicio/app/src/main/java/com/example/inicio/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.inicio
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +11,30 @@ import com.example.inicio.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var adapterPerfiles: ArrayAdapter<CharSequence>
+    private lateinit var listaPerfiles: List<CharSequence>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        instances()
+        initGUI()
 
+    }
+
+    private fun instances() {
+        listaPerfiles = listOf("Admin", "Usuario", "Invitado")
+        adapterPerfiles =
+            ArrayAdapter(applicationContext, android.R.layout.simple_spinner_item, listaPerfiles)
+
+
+    }
+
+
+    private fun initGUI() {
+        // List -> relleno y asocio
+        binding.spinnerPerfiles.adapter = adapterPerfiles;
+        adapterPerfiles.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
     }
 }
