13a44656d9f7fd136c799d718923f8dbcaf4d261
Develop and System <developandsystem@gmail.com>
2026-03-09 11:13:23 +0100

    update

---
+++ 2526/INSOBC/TEMA3/inicio/app/src/main/java/com/example/inicio/RegistroActivity.kt
@@ -0,0 +1,19 @@
+package com.example.inicio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inicio.databinding.ActivityRegistroBinding
+
+class RegistroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
