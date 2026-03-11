950259dab93807a75d296a2de404f85489552d13
Develop and System <developandsystem@gmail.com>
2026-03-11 11:21:19 +0100

    update

---
+++ 2526/INSOD/TEMA3/inicio/app/src/main/java/com/example/inicio/LoginActivity.kt
@@ -0,0 +1,98 @@
+package com.example.inicio
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inicio.databinding.ActivityLoginBinding
+import com.example.inicio.model.User
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var adapterProfile: ArrayAdapter<CharSequence>
+    private lateinit var listProfiles: List<CharSequence>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        instances()
+        initGUI()
+        actions()
+    }
+
+    private fun actions() {
+
+        binding.botonLogin.setOnClickListener {
+            val intent: Intent = Intent(applicationContext, MainActivity::class.java)
+            val mail= binding.editCorreo.text.toString()
+            val pass=binding.editPass.text.toString();
+            val profile = binding.spinnerPerfil.selectedItem.toString()
+            val remeber = binding.checkPass.isChecked
+            val user = User(mail,pass,profile,remeber)
+            intent.putExtra("user_data",user)
+            startActivity(intent)
+            clearData()
+        }
+    }
+
+    private fun clearData() {
+
+        binding.editCorreo.text.clear()
+        binding.editPass.text.clear()
+        binding.checkPass.isChecked = false
+        binding.spinnerPerfil.setSelection(0)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        clearData()
+    }
+
+    private fun initGUI() {
+        binding.spinnerPerfil.adapter = adapterProfile
+        adapterProfile.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    }
+
+    private fun instances() {
+        listProfiles = listOf("Administrador", "Usuario", "Invitado")
+        adapterProfile = ArrayAdapter<CharSequence>(
+            applicationContext,
+            android.R.layout.simple_spinner_item, listProfiles
+        )
+    }
+
+    /*
+    En la pantalla de login
+        Si pulso login
+            si falta algun dato por rellenar -> aviso del error
+            si todos los datos estan rellenos
+                si el user/pass admin@admin.com admin-> navega
+                si el user/pass no es admin@admin.com admin -> avisto del error
+     */
+
+    /*
+    Si doy al boton de registro
+        Nueva pantalla donde tengo un form
+            Nombre
+            Apellido
+            Correo
+            Pass
+            Pass verificada
+            Edad (17-99)
+            Boton
+
+            Al darle al boton puede pasar lo siguiente
+                Si falta algun campo -> aviso del error
+                Si las pass no coinciden -> aviso del error
+                En caso contrario
+                    Navego al login, pansando el usuario
+                        en el campo mail -> mail
+                        en el campo pass -> pass
+     */
+}
---
+++ 2526/INSOD/TEMA3/inicio/app/src/main/java/com/example/inicio/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.inicio
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.inicio.databinding.ActivityMainBinding
+import com.example.inicio.model.User
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    // primer metodo del ciclo de vida
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var user: User
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getData()
+        initGUI()
+        actions()
+    }
+
+    private fun initGUI() {
+        binding.editNombre.setText(user.mail)
+    }
+
+    private fun getData() {
+        user = intent.getSerializableExtra("user_data") as User
+    }
+
+    private fun actions() {
+
+        /*
+        binding.botonVaciar.setOnClickListener {
+            binding.editNombre.text.clear()
+        }
+        binding.botonSaludar.setOnClickListener {
+
+            if (binding.editNombre.text.isEmpty()) {
+                showSnack(resources.getString(R.string.text_snack_empty))
+            } else {
+                showSnack("Enhorabuena ${binding.editNombre.text.toString()}, reto comletado")
+            }
+        }
+        */
+
+        binding.botonSaludar.setOnClickListener(this)
+        binding.botonVaciar.setOnClickListener(this)
+        binding.botonSalir?.setOnClickListener(this)
+    }
+
+    private fun showSnack(text: String) {
+        val snack = Snackbar.make(
+            binding.root,
+            text,
+            Snackbar.LENGTH_LONG
+        )
+        snack.show()
+    }
+
+    override fun onClick(v: View?) {
+        // funcionamiento comun
+        when (v?.id) {
+            binding.botonVaciar.id -> {
+
+            }
+
+            binding.botonSaludar.id -> {
+
+            }
+
+            binding.botonSalir!!.id -> {
+                finish()
+            }
+        }
+    }
+}
