a4ec184a474eb80678fa64ec8cd740f2cbe983b9
Develop and System <developandsystem@gmail.com>
2026-03-18 12:49:26 +0100

    update

--- 2526/INSOD/TEMA3/inicio/app/src/main/java/com/example/inicio/ListaActivity.kt
+++ 2526/INSOD/TEMA3/inicio/app/src/main/java/com/example/inicio/ListaActivity.kt
@@ -5,15 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.inicio.adapter.ProductAdapter
+import com.example.inicio.data.DataSet
 import com.example.inicio.databinding.ActivityListaBinding
 
 class ListaActivity : AppCompatActivity() {
-
+    private lateinit var productAdapter: ProductAdapter
     private lateinit var binding: ActivityListaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListaBinding.inflate(this.layoutInflater)
         setContentView(binding.root)
+        productAdapter = ProductAdapter(DataSet.getAllProducts(), this)
+        binding.recyclerProductos.adapter = productAdapter
+        binding.recyclerProductos.layoutManager = GridLayoutManager(
+            this,
+            2,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
 
     }
 }
