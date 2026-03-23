7a5bf361bb421e3a226a90cbb473dd48b6693a72
Develop and System <developandsystem@gmail.com>
2026-03-23 10:47:10 +0100

    update

--- 2526/INSOA/TEMA4/Inicio/app/src/main/java/com/example/inicio/ListaActivity.kt
+++ 2526/INSOA/TEMA4/Inicio/app/src/main/java/com/example/inicio/ListaActivity.kt
@@ -1,6 +1,7 @@
 package com.example.inicio
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +9,22 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.inicio.adapter.ProductoAdapter
 import com.example.inicio.data.DataSet
 import com.example.inicio.databinding.ActivityListaBinding
+import com.example.inicio.model.ProductJSON
+import com.google.gson.Gson
+import org.json.JSONArray
+import org.json.JSONObject
 
 class ListaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListaBinding
     private lateinit var adapterProducto: ProductoAdapter
+    private lateinit var listJSON: ArrayList<ProductJSON>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,28 +32,65 @@ class ListaActivity : AppCompatActivity() {
         setContentView(binding.root)
         instances()
         initGUI()
+        getJSONProducts()
+
+
         // binding.recyclerProductos
     }
+    private fun getJSONProducts() {
+        // 1: creo la peticion
+        val gson = Gson()
+        val request: JsonObjectRequest = JsonObjectRequest(
+            "https://dummyjson.com/product", {
+                val products: JSONArray = it.getJSONArray("products")
+                for (i in 0..products.length() - 1) {
+                    val jsonProduct: JSONObject = products.getJSONObject(i)
+                    val product = gson.fromJson(
+                        jsonProduct.toString(),
+                        ProductJSON::class.java
+                    )
+                    Log.v("datos",product.title.toString())
+                    adapterProducto.addProduct(product)
+                }
 
+            },
+            {
+                Log.v("datos",it.toString())
+            })
+        // 2: lanzo la peticion
+        Volley.newRequestQueue(applicationContext).add(request)
+    }
     private fun initGUI() {
         binding.recyclerProductos.adapter = adapterProducto
         // como internamente vas a renderizar la lista
 
-        if (resources.configuration.orientation == 1){
-            binding.recyclerProductos.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        if (resources.configuration.orientation == 1) {
+            binding.recyclerProductos.layoutManager =
+                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         } else {
-            binding.recyclerProductos.layoutManager = GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false)
+            binding.recyclerProductos.layoutManager =
+                GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false)
         }
 
         //
         //
     }
-
     private fun instances() {
+        listJSON = ArrayList()
         adapterProducto = ProductoAdapter(
-            DataSet.getAllProducts(),
             this
         )
+
         // si quieres usar interfaz de callback -> this en vez de applicationContext
     }
 }
+
+
+/*
+Al pulsar el boton de ver detalle
+    se carga una nueva pantalla con los datos del producto seleccionado
+Al pulsar el boton comprar, el producto se agrega a un carrito (meterlo dentro del dataset)
+    incorporar un boton en la pantalla de ver total, que muestra una snackbar con el total del carrito
+Poner un spinner con las categorias (sacadas del API). Al cambiar de categoria
+    se filtrara la lista de productos, mostrando los que son de dicha categoria
+ */
--- 2526/INSOA/TEMA4/Inicio/app/src/main/java/com/example/inicio/adapter/ProductoAdapter.kt
+++ 2526/INSOA/TEMA4/Inicio/app/src/main/java/com/example/inicio/adapter/ProductoAdapter.kt
@@ -9,15 +9,18 @@ import com.bumptech.glide.Glide
 import com.example.inicio.R
 import com.example.inicio.databinding.ItemProductoBinding
 import com.example.inicio.model.Product
+import com.example.inicio.model.ProductJSON
 import kotlin.random.Random
 
 // 1- extiendo de Recycler.Adater
 // 3- Tipo el adapter con la clase anidada
 // 4- implmento dos los metodos de la clase
 // 5- por constructor pasa todos los parametros que son necesarios
-class ProductoAdapter(var lista: ArrayList<Product>, var context: Context) :
+class ProductoAdapter(var context: Context) :
     RecyclerView.Adapter<ProductoAdapter.MyHolder>() {
     // 2 - creo clase anidada inner : RecycerView.ViewHolder
+    val lista: ArrayList<ProductJSON> = ArrayList()
+
     inner class MyHolder(var binding: ItemProductoBinding) : RecyclerView.ViewHolder(binding.root)
 
     // crea la plantilla de las filas
@@ -43,7 +46,7 @@ class ProductoAdapter(var lista: ArrayList<Product>, var context: Context) :
         // holder.binding.imagenProducto.setImageResource(R.drawable.imgdefault)
         // TODO pendiente pintar la imagen con Glide
         Glide.with(context)
-            .load(product.image)
+            .load(product.thumbnail)
             .placeholder(R.drawable.imgdefault)
             .into(holder.binding.imagenProducto)
         // TODO pendiente de decir como funcionan los botones
@@ -54,6 +57,11 @@ class ProductoAdapter(var lista: ArrayList<Product>, var context: Context) :
     // cuantos elementos tiene la lista de "cosas" a renderiza
     override fun getItemCount(): Int {
         return lista.size
+    }
+
+    fun addProduct(productJSON: ProductJSON) {
+        this.lista.add(productJSON)
+        notifyItemInserted(lista.size - 1)
     }
 
 
