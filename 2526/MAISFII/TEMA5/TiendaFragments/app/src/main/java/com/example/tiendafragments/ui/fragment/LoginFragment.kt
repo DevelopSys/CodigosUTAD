547d769e44ec735093555a615d0cecd45f54f87f
Develop and System <developandsystem@gmail.com>
2026-04-15 10:17:17 +0200

    update

---
+++ 2526/MAISFII/TEMA5/TiendaFragments/app/src/main/java/com/example/tiendafragments/ui/fragment/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.example.tiendafragments.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tiendafragments.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+
+}
