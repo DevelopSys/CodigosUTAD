ffad49e5ad8e4e696800290e6a8ac09a541432df
Develop and System <developandsystem@gmail.com>
2026-04-30 20:18:08 +0200

    update

---
+++ 2526/INSONLINE/TEMA2/FragmentsFP/app/src/main/java/com/example/fragmentsfp/SecondFragment.kt
@@ -0,0 +1,46 @@
+package com.example.fragmentsfp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fragmentsfp.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            val bundle = Bundle();
+            bundle.putString("tag1","dato")
+            findNavController().navigate(R.id.action_SecondFragment_to_thirdFragment,bundle)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ 2526/INSONLINE/TEMA2/FragmentsFP/app/src/main/java/com/example/fragmentsfp/ThirdFragment.kt
@@ -0,0 +1,52 @@
+package com.example.fragmentsfp
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fragmentsfp.databinding.FragmentSecondBinding
+import com.example.fragmentsfp.databinding.FragmentThirdBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private lateinit var tag: String
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        tag = requireArguments().getString("tag1").toString()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNav.setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
